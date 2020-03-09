aa1df5429f9cfd8c204d60df94ceca5c876de0a5
climber1982 <403783154@qq.com>
2020-03-09 21:43:12 +0800

    init

--- springmvc/src/main/java/com/lovo/spring/controller/UserController.java
+++ springmvc/src/main/java/com/lovo/spring/controller/UserController.java
@@ -5,9 +5,13 @@ import com.lovo.spring.entity.TeacherEntity;
 import com.lovo.spring.vo.UserVO;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.List;
 
 //整个类的方法不会再做视图渲染，只返回数据
